b3d60e00517d870ace50ff13972ce77161b54b9f
Mats Larsen <me@supergrecko.com>
2021-03-20 15:50:42 +0100

    fix: properly export testing utilities

--- src/main/kotlin/org/llvm4j/optional/testing/Assertions.kt
+++ src/main/kotlin/org/llvm4j/optional/testing/Assertions.kt
@@ -3,7 +3,7 @@ package org.llvm4j.optional.testing
 import org.llvm4j.optional.Option
 import org.llvm4j.optional.Result
 
-internal fun <T> assertNone(opt: Option<T>) = assert(opt.isNone()) { "Expected $opt to be None" }
-internal fun <T> assertSome(opt: Option<T>) = assert(opt.isSome()) { "Expected $opt to be Some" }
-internal fun <T, E> assertOk(res: Result<T, E>) = assert(res.isOk()) { "Expected $res to be Ok" }
-internal fun <T, E> assertErr(res: Result<T, E>) = assert(res.isErr()) { "Expected $res to be Err" }
+public fun <T> assertNone(opt: Option<T>): Unit = assert(opt.isNone()) { "Expected $opt to be None" }
+public fun <T> assertSome(opt: Option<T>): Unit = assert(opt.isSome()) { "Expected $opt to be Some" }
+public fun <T, E> assertOk(res: Result<T, E>): Unit = assert(res.isOk()) { "Expected $res to be Ok" }
+public fun <T, E> assertErr(res: Result<T, E>): Unit = assert(res.isErr()) { "Expected $res to be Err" }
